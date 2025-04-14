e93a517e945ad033333097cefadc593c413bfb1a
Sophia Dolan <25srdolan@gmail.com>
2025-04-13 20:41:44 -0400

    /Task 4 - Handle loading and error states gracefully

--- vite-project/src/components/gallery.jsx
+++ vite-project/src/components/gallery.jsx
@@ -1,11 +1,18 @@
 // Task 2 - Fetch and render tour list with useEffect and state
+// Task 4 - Handle loading and error states gracefully
 
-import React, { useEffect } from "react"; 
-// imports react and uses useEffect
+import React, { useEffect, useState } from "react"; 
+// imports react and uses useEffect and useState
 import TourCard from "./TourCard"; 
 //imported the TourCard
 
 const Gallery = ({ tours, setTours, onRemove }) => {
+  const [loading, setLoading] = useState(true); 
+  //  tracks if it is still loading
+
+  const [error, setError] = useState(false); 
+  // tracks if something broke
+
   useEffect(() => {
     //runs when it loads 
     const fetchTours = async () => {
@@ -19,13 +26,40 @@ const Gallery = ({ tours, setTours, onRemove }) => {
          // sets the tours and puts them in states
       } catch (error) {
         console.log("error getting tours", error); 
-        // just logs if it breaks and there is an error 
+        // logs if it breaks and there is an error 
+
+        setError(true); 
+        // if it fails, rror to true
+      } finally {
+        setLoading(false); 
+        // done loading 
       }
     };
 
     fetchTours();
      // runs when page loads
   }, [setTours]);
+
+  if (loading) {
+    return <h2>Loading...</h2>; 
+    // shows loading
+  }
+
+  if (error) {
+    return <h2>Something went wrong.</h2>; 
+    //  shows if the fetch fails
+  }
+
+  if (tours.length === 0) {
+    return (
+      <div className="no-tours">
+        <h2>No tours left</h2>
+        <button onClick={() => window.location.reload()}>
+          Refresh
+        </button>
+      </div>
+    );
+  }
 
   return (
     <section className="tour-list">
